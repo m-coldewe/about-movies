40878a4240d738b09496d27606819cc853bc1dc9
Meagan Coldewe <yamoriami@yahoo.com>
2024-04-04 19:12:09 -0400

    third try's the charm

--- app.py
+++ app.py
@@ -6,26 +6,24 @@ from dash_bootstrap_templates import load_figure_template
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG], use_pages=True)
 
+# app.layout = dbc.Container([
+#     dbc.Row([
+#         dbc.Col(html.H1('about-movies'),
+#                 width=2),
+#         dbc.Col(Nav = dbc.Nav(
+#     [
+#         dbc.NavItem(dbc.NavLink("Home", active="exact", href="/")),
+#         dbc.NavItem(dbc.NavLink("Map", active="exact", href="/map")),
+#         dbc.NavItem(dbc.NavLink("Profit", active="exact", href="/profit")),
+#         dbc.NavItem(dbc.NavLink("Disabled", disabled=True, href="#")),
+#     ],
+#     pills=True,
+# ))
+#     ]),
 
+# ])
 
 #app = Dash(__name__, use_pages=True)
-
-app.layout = html.Div([
-    html.Div(
-        className="app-header",
-        children=[
-            html.Div("Plotly Dash", className="app-header--title")
-        ]
-    ),
-    html.Div(
-        children=html.Div([
-            html.H1("Overview"),
-            html.Div('''
-                This is exceptionally frustrating.
-            ''')
-        ])
-    )
-])
 
 
 nav = dbc.Nav(
@@ -38,13 +36,17 @@ nav = dbc.Nav(
     pills=True,
 )
 
-
-
-app.layout = html.Div([
-    html.H1('about-movies'),
+app.layout = dbc.Container([
     nav,
     dash.page_container
-])
+    ])
+
+
+# app.layout = dbc.Container([
+#     nav,
+#     dash.page_container,
+#     html.H1('about-movies')])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- pages/home.py
+++ pages/home.py
@@ -1,10 +1,60 @@
 import dash
-from dash import html
+from dash import html, Dash, html, dcc
+import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import load_figure_template
 
 
 dash.register_page(__name__, path='/')
 
-layout = html.Div([
-    html.H1('This is our Home page'),
+
+layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(html.H2("Welcome to about-movies:",
+                        className='text-center text-primary'),
+                        width=12)
+    ]),
+    dbc.Row([
+        dbc.Col(html.H2("A Data Visualization Project",
+                        className='text-center text-primary, mb-4'),
+                        width=12)
+    ]),
+    dbc.Row([
+        dbc.Col(html.H4("By perfectionists-under-pressure",
+                        className='text-center text-primary, mb-4'),
+                        width=12)
+    ]),
+    dbc.Row([
+        dbc.Col([
+            dbc.Carousel(
+    items=[
+        {"key": "1", "src": "/assets/test.png"},
+        {"key": "2", "src": "/assets/test.png"},
+        {"key": "3", "src": "/assets/test.png"},
+    ],
+    controls=False,
+    indicators=False,
+    interval=3000,
+    ride="carousel",
+)
+        ])
+    ])
+])
+
+
+html.Div([
+    html.H1('about-movies'),
     html.Img(src=dash.get_asset_url('test.png')),
 ])
+
+
+carousel = dbc.Carousel(
+    items=[
+        {"key": "1", "src": "/static/images/slide1.svg"},
+        {"key": "2", "src": "/static/images/slide2.svg"},
+        {"key": "3", "src": "/static/images/slide3.svg"},
+    ],
+    controls=False,
+    indicators=False,
+    interval=2000,
+    ride="carousel",
+)
