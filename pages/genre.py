8d43814241aafcff1aca17a5302e7dae2fee18f0
Meagan Coldewe <yamoriami@yahoo.com>
2024-04-06 13:23:28 -0400

    update, still incomplete

--- pages/genre.py
+++ pages/genre.py
@@ -18,9 +18,11 @@ df = pd.read_sql(query, conn)
 # close the database connection
 conn.close()
 
-genres = df[['genre_1', 'genre_2', 'genre_3']].stack().reset_index(drop=True)
+genres = df[['genre_1', 'genre_2', 'genre_3']].stack().unique()
 
 dash.register_page(__name__, path='/genre')
+
+initial_value = list(genres)
 
 # define the layout
 layout = dbc.Container([
@@ -29,10 +31,10 @@ layout = dbc.Container([
     ]),
     dbc.Row([
         dbc.Col([
-            dcc.RadioItems(
+            dbc.Checklist(
                 id = 'genre-radio',
-                options = [{'label': genre, 'value': genre} for genre in genres.unique()],
-                value='action', 
+                options = [{'label': genre, 'value': genre} for genre in genres],
+                value=initial_value, 
                 inline=True,
             )
         ])
@@ -47,12 +49,31 @@ layout = dbc.Container([
 
 # define the callback to update the graph based on the selected genres
 @app.callback(
-    Output('genre-graph', 'figure'),
+    Output('genre-graph', 'fig'),
     [Input('genre-radio', 'value')],
 )
 
-def update_genre_graph(selected_genres):
-    filtered_df = df[df[['genre_1', 'genre_2', 'genre_3']].isin(selected_genres).any(axis=1)]
-    fig = px.line(filtered_df.groupby('release_year').size().reset_index(name='count'), 
-                  x = 'release_year', y = 'count', title = 'Genre Through the Years')
-    return fig
+# def update_genre_graph(selected_genres):
+#     filtered_df = df[df[['genre_1', 'genre_2', 'genre_3']].isin(selected_genres).any(axis=1)]
+#     fig = px.line(filtered_df.groupby('release_year').size().reset_index(name='count'), 
+#                   x = 'release_year', y = 'count', title = 'Genre Through the Years')
+#     return fig
+
+def update_genre_graph(n_clicks):
+
+    if isinstance(n_clicks, str):
+        n_clicks = [n_clicks]
+
+    # if no genres, return empty figure
+    if not n_clicks:
+        return px.line(), {'display': 'none'}
+    
+    filtered_df = df[df[['genre_1', 'genre_2', 'genre_3']].apply(lambda row: any(genre in n_clicks for genre in row).count(),axis=1)]
+
+    if filtered_df.empty:
+        return px.line(), {'display': 'none'}
+    
+    # plot the graph based on the filtered dataframe
+    fig = px.line(filtered_df.groupby('release_year').size().reset_index(name='count'),
+                 x='release_year', y='count', title='Genres Through the Years' )
+    return fig, {}
